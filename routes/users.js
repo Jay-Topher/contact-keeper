2c1fd0a9d7eaccd471a159d2d4ac381e9e413bb6
Jones Ogolo <jay.topher11@gmail.com>
2019-12-23 22:40:04 +0100

    added express validation

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 /**
  * @route       POST api/users
  * @description Register a user
  * @access      Piblic
  */
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.get(
+  "/",
+  [
+    check("name", "name is required")
+      .not()
+      .isEmpty(),
+
+    check('email', 'Please input a valid mail').isEmail()
+  ],
+  (req, res) => {
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
