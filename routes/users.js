1562addb707253c9c02eb78b0f8290295acf3420
Jones Ogolo <jay.topher11@gmail.com>
2019-12-24 18:56:50 +0100

    saved user and hashed the password before saving

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs')
 
 const User = require("../models/User");
 
@@ -9,7 +10,7 @@ const User = require("../models/User");
  * @description Register a user
  * @access      Piblic
  */
-router.get(
+router.post(
   "/",
   [
     check("name", "Please add name")
@@ -22,8 +23,42 @@ router.get(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
-    res.send(req.body);
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+
+      return;
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ msg: "User already exists" });
+
+        return;
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+
+      res.send("User Saved")
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).json({msg: 'server error'})
+    }
   }
 );
 
