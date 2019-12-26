c33e08f6a0dca777f02c0540adb569d38a2f171c
Jones Ogolo <jay.topher11@gmail.com>
2019-12-26 19:53:31 +0100

    added functionality to get logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const auth = require('../middleware/auth')
+const User = require("../models/User");
 
 /**
  * @route       GET api/auth
  * @description Get logged in user
  * @access      Private
  */
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user)
+
+    return;
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 });
 
 /**
