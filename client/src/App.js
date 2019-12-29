68a3a997796602210ef2be954d4e72d00f28da84
Jones Ogolo <jay.topher11@gmail.com>
2019-12-29 23:06:38 +0100

    implemented routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
 function App() {
   return (
-    <div className="App">
-      My App
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
 }
 
