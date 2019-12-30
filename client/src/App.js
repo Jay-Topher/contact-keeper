18216fc874ae491c7f96239c175d70813f053630
Jones Ogolo <jay.topher11@gmail.com>
2019-12-30 23:21:53 +0100

    added contact state

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Home from './components/pages/Home'
-import About from './components/pages/About'
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <ContactState>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContactState>
   );
 }
 
