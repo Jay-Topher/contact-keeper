c8c576f91618e833dcc09ba2da763beaa436102f
Jones Ogolo <jay.topher11@gmail.com>
2020-01-06 21:41:21 +0100

    added props.children to authcontext

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -21,30 +21,31 @@ const AuthState = props => {
     loading: true,
     error: null
   };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  // Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
-
-const [state, dispatch] = useReducer(authReducer, initialState);
-
-// Load User
-
-// Register User
-
-// Login User
-
-// Logout
-
-// Clear Errors
-
-return (
-  <AuthContext.Provider value={{
-    token: state.token,
-    isAuthenticated: state.isAuthenticated,
-    loading: state.loading,
-    user: state.user,
-    error: state.error
-  }} >
-
-  </AuthContext.Provider>
-)
-
-export default AuthState
+export default AuthState;
